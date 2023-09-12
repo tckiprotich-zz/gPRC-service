d7f8de39106e51780e32685104c0534c60850ccf
Ubuntu <azureuser@iphurts.5j4nmskja0cuvnnhdtizwfiqha.bx.internal.cloudapp.net>
2023-09-12 16:25:03 +0000

    gRPC JSON transcoded the gPRC to REST APIs

--- gPRC_API/Program.cs
+++ gPRC_API/Program.cs
@@ -11,13 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=app.db"));
 
 
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<ToDoService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
